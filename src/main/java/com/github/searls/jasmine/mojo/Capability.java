236d2b246026677b140f74d1aa0c4c3e3a899c9d
Deena Wang <deena.wang@vecna.com>
2014-12-15 15:40:47 -0500

    Adds ability to configure arbitrary chromeOptions
    Code acquired from https://github.com/searls/jasmine-maven-plugin/pull/223

--- src/main/java/com/github/searls/jasmine/mojo/Capability.java
+++ src/main/java/com/github/searls/jasmine/mojo/Capability.java
@@ -1,5 +1,6 @@
 package com.github.searls.jasmine.mojo;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -39,6 +40,24 @@ public class Capability {
   }
 
   public void setMap(Map<String, Object> map) {
+    for (String key : map.keySet()) {
+      String valueStr = (String) map.get(key);
+      if (isListString(valueStr)) {
+        List<String> listValue = Arrays.asList(valueStr.substring(1, valueStr.length() - 1).split(","));
+        map.put(key, listValue);
+      } else if (isBooleanString(valueStr)) {
+        Boolean booleanValue = Boolean.parseBoolean(valueStr);
+        map.put(key, booleanValue);
+      }
+    }
     this.map = map;
+  }
+
+  private boolean isListString(String str) {
+    return str.charAt(0) == '[' && str.charAt(str.length() - 1) == ']';
+  }
+
+  private boolean isBooleanString(String str) {
+    return "true".equals(str) || "false".equals(str);
   }
 }
